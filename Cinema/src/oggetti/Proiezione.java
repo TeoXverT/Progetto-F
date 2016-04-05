95f6d1c7d8d23b00b9bc16716c7687b47031b22d
Yoga <Yoga@10.87.246.0>
2016-04-05 16:54:57 +0200

    Correzione biooraria idrodinamica 2

--- Cinema/src/oggetti/Proiezione.java
+++ Cinema/src/oggetti/Proiezione.java
@@ -1,5 +1,7 @@
 package oggetti;
 
+import java.util.Calendar;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,7 +14,7 @@ package oggetti;
  */
 public class Proiezione {
     private int id_protezione;
-    private int data_ora;
+    private Calendar data_ora;
     private int id_film;
     private int id_sala;
     
