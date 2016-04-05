8b0171c3d8a21727bab537454694770b3ca60867
Yoga <Yoga@DESKTOP-VHDGSJM>
2016-04-05 21:36:58 +0200

    Test commit su ramo

--- Cinema/src/main.java
+++ Cinema/src/main.java
@@ -7,7 +7,7 @@ import oggetti.*;
 public class main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Progetto Cinema V 0.1/W la mozzarella");
+        System.out.println("Progetto Cinema V 0.2/W la mozzarella");
 
         IODati IO = new IODati();
 
