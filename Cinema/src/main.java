e6e5c7526fea7d837de30a24f6830878d37015f5
cri <cri@cri-pc>
2016-04-12 17:49:03 +0200

    CLIENTE

--- Cinema/src/main.java
+++ Cinema/src/main.java
@@ -1,13 +1,17 @@
 
+import Cliente.Cliente;
 import input_output.*;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 import oggetti.*;
 
 public class main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Progetto Cinema V 0.1/W la pasta ");
+      /*  System.out.println("Progetto Cinema V 0.1/W la pasta ");
 
         Input input = new Input();
 
@@ -42,8 +46,30 @@ public class main {
         System.out.println( output.caricaProiezione(listaProiezione,"file_db/proiezione_copy.txt") );
         System.out.println( output.caricaPrenotazione(listaPrenotazione,"file_db/prenotazione_copy.txt") );
 
-         
-
+        */
+        /*
+        
+        
+        
+        ArrayList<Proiezione> listaProiezioni;
+         ArrayList<Proiezione> listaProiezioniFuture;
+        Cliente cliente = new Cliente();
+        Input input = new Input();
+        int i ;
+        
+        listaProiezioni = cliente.listaProiezioniFuture( input.scaricaProiezione("file_db/proiezione.txt"));
+        
+        
+        
+          listaProiezioniFuture =   cliente.listaProiezioniFuture(listaProiezioni);
+        
+        for(i = 0; i < listaProiezioniFuture.size(); i++) {
+            
+            System.out.println(listaProiezioniFuture.get(i));
+            
+        }
+        
+                */
     }
     
     
