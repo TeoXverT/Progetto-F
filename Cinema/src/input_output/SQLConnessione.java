5a8998fd1318f89000f1b6fffb2ff14e515da6a6
Yoga <Yoga@DESKTOP-VHDGSJM>
2016-04-22 00:31:52 +0200

    Creazione Connettore Al Database SQL

---
+++ Cinema/src/input_output/SQLConnessione.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package input_output;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SQLConnessione {
+
+    private java.sql.Connection conn;
+    private Statement stmt;
+
+    private final String url = "sql8.freemysqlhosting.net";
+    private final String nomeDatabase = "sql8115909";
+    private final String user = "sql8115909";
+    private final String pass = "ifYmYwRJJS";
+
+    public SQLConnessione() {
+
+    }
+
+    ResultSet eseguiQuery(String SQL) throws SQLException { //Necessita di stetmet try chatch esterno
+        return stmt.executeQuery(SQL);
+    }
+
+    boolean creaConnessione() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection("jdbc:mysql://" + url + ":3306/" + nomeDatabase + "?user=" + user + "&password=" + pass);
+            stmt = conn.createStatement();
+            return true;
+
+        } catch (SQLException E) {
+            System.err.println("SQLException: " + E.getMessage());
+            System.err.println("SQLState:     " + E.getSQLState());
+            System.err.println("VendorError:  " + E.getErrorCode());
+            return false;
+        } catch (Exception E) {
+            System.err.println("Non trovo il driver da caricare.");
+            E.printStackTrace();
+            return false;
+        }
+    }
+
+    boolean chiudiConnessione() {
+        try {
+            stmt.close();
+            conn.close();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(SQLConnessione.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+}
