c4786f2da4bf5f630b1989539ab9b2a41e7cefbd
umeer <umeermohammad@gmail.com>
2016-05-01 00:55:07 +0200

    //LE MODIFICHE INGENTI RIGUARDANO I SEGUENTI PUNTI
            0) HO MODIFICATO LE WIKI SU GITHUB METTENDO DEI SCREENSHOOT IPOTETICHI (PHOTOSHOP A MANETTA) CHE RAPPRESENTANO IN MINIMA PARTE IL RISULTATO FINALE
            0) NON SO PERCHE' STO SCRIVENDO IN MAIUSCOLO, MA ORA MAI HO GIA SCRITTO 2 RIGHE E NON MI VA DI CANCELLARLE E RISCRIVERE TUTTO
            1) I CONTROLLER_GESTORE E IL CONTROLLER_CLIENTE SI SONO FUSI IN UN UNICA CLASSE CHIAMATA ADAPTER SQL
            2) LE CLASSI CLIENTE E GESTORE ORA SI CHIAMANO RISPETTIVAMENTE CONTROLLER_CLIENTE E CONTROLLER_GESTORE (PRIMA AVEVANO UN NOME IMPROPRIO
            3) CI SONO STATE MODIFICHE MINORI AD ALCUNE CLASSI IN OGGETTI (NULLA DI RILEVANTE)
            4) IN SQLCONNESSIONE IL METODO ESEGUI_QUERY SI E' SUDDIVISO IN DUE METODI eseguiQueryScrittura E eseguiQueryLettura CHE COME RICRDA ANCHE IL NOME SI USANO PER DUE MOTIVI DIVERSI
            5) LA GUI_GESTORE ORA E' DISEGNATA DA OGGETTI ESTERNI (JPANEL), QUESTO AL FINE DI RENDERE IL CODE PULITO, FLUIDO E FACILE DA MODIFICARE
            
    Merge origin/impostazioni
    
    Conflicts:
    	Cinema/src/Gestore/Gestore.java
    	Cinema/src/Gestore/Gui_Gestore.java
    	Cinema/src/input_output/Controller_Dati_Gestore.java
    	Cinema/src/input_output/Input.java
    	Cinema/src/input_output/Output.java
    	Cinema/src/main.java

--- Cinema/src/Cliente/Controller_Cliente.java
+++ Cinema/src/Cliente/Controller_Cliente.java
@@ -17,85 +17,64 @@ import oggetti.Proiezione;
  * @author Yoga
  */
 public class Controller_Cliente {
-    
+
     ArrayList<Proiezione> listaProiezioniFuture;
     ArrayList<Proiezione> listaProiezioniFiltrate;
-    
-    
-    public Controller_Cliente() {
-        
-        
-        
-    }
-    
-    
-  public ArrayList<Proiezione> listaProiezioniFuture(ArrayList<Proiezione>  listaProiezione) {
-      int i;
-      Calendar giornoAttuale;
-      TimeZone timezone = TimeZone.getTimeZone("Europe/Rome");
-      Proiezione proiezioneProva;
-      
-      listaProiezioniFuture = new ArrayList<>();
-      
-      giornoAttuale = Calendar.getInstance(timezone);
 
-      for(i = 0; i < listaProiezione.size(); i++) {
-          
-          if(listaProiezione.get(i).getData_ora().after(giornoAttuale)) {
-              
-              proiezioneProva = new Proiezione(listaProiezione.get(i).getId_proiezione(), listaProiezione.get(i).getData_ora(), listaProiezione.get(i).getId_film(), listaProiezione.get(i).getId_sala(), listaProiezione.get(i).getTipo_proiezione(), listaProiezione.get(i).getPrezzo());
-              
-              listaProiezioniFuture.add(proiezioneProva);
-              
-          }
-          
-      }
-      
-      return listaProiezioniFuture;
-  }
-  
-  
-  
-  public ArrayList<Proiezione> listaProiezioniFiltrate(ArrayList<Proiezione> listaProiezione, int GiornoSettimana) {
-      
-       int i;
-      Calendar giornoAttuale;
-      
-      
-      TimeZone timezone = TimeZone.getTimeZone("Europe/Rome");
-      Proiezione proiezioneProva;
-      
-      listaProiezioniFiltrate = new ArrayList<>();
-      
-      giornoAttuale = Calendar.getInstance(timezone);
-      
-      
-              
-      
-      
-      
-      
-      for(i = 0; i < listaProiezione.size(); i++) {
-          System.out.println(listaProiezione.get(i).getData_ora().get(Calendar.DAY_OF_WEEK));
-          if(listaProiezione.get(i).getData_ora().after(giornoAttuale) && (listaProiezione.get(i).getData_ora().get(Calendar.DAY_OF_WEEK)) == GiornoSettimana ) {
-              
-              proiezioneProva = new Proiezione(listaProiezione.get(i).getId_proiezione(), listaProiezione.get(i).getData_ora(), listaProiezione.get(i).getId_film(), listaProiezione.get(i).getId_sala(), listaProiezione.get(i).getTipo_proiezione(), listaProiezione.get(i).getPrezzo());
-              
-              listaProiezioniFiltrate.add(proiezioneProva);
-              
-              
-          }
-      
-      
-      
-      
-      
-  }
-  
-  
-    
-   
-     return listaProiezioniFiltrate;
-}
-  
+    public Controller_Cliente() {
+
+    }
+
+    public ArrayList<Proiezione> listaProiezioniFuture(ArrayList<Proiezione> listaProiezione) {
+        int i;
+        Calendar giornoAttuale;
+        TimeZone timezone = TimeZone.getTimeZone("Europe/Rome");
+        Proiezione proiezioneProva;
+
+        listaProiezioniFuture = new ArrayList<>();
+
+        giornoAttuale = Calendar.getInstance(timezone);
+
+        for (i = 0; i < listaProiezione.size(); i++) {
+
+            if (listaProiezione.get(i).getData_ora().after(giornoAttuale)) {
+
+                proiezioneProva = new Proiezione(listaProiezione.get(i).getId_proiezione(), listaProiezione.get(i).getData_ora(), listaProiezione.get(i).getId_film(), listaProiezione.get(i).getId_sala(), listaProiezione.get(i).getTipo_proiezione(), listaProiezione.get(i).getPrezzo());
+
+                listaProiezioniFuture.add(proiezioneProva);
+
+            }
+
+        }
+
+        return listaProiezioniFuture;
+    }
+
+    public ArrayList<Proiezione> listaProiezioniFiltrate(ArrayList<Proiezione> listaProiezione, int GiornoSettimana) {
+
+        int i;
+        Calendar giornoAttuale;
+
+        TimeZone timezone = TimeZone.getTimeZone("Europe/Rome");
+        Proiezione proiezioneProva;
+
+        listaProiezioniFiltrate = new ArrayList<>();
+
+        giornoAttuale = Calendar.getInstance(timezone);
+
+        for (i = 0; i < listaProiezione.size(); i++) {
+            System.out.println(listaProiezione.get(i).getData_ora().get(Calendar.DAY_OF_WEEK));
+            if (listaProiezione.get(i).getData_ora().after(giornoAttuale) && (listaProiezione.get(i).getData_ora().get(Calendar.DAY_OF_WEEK)) == GiornoSettimana) {
+
+                proiezioneProva = new Proiezione(listaProiezione.get(i).getId_proiezione(), listaProiezione.get(i).getData_ora(), listaProiezione.get(i).getId_film(), listaProiezione.get(i).getId_sala(), listaProiezione.get(i).getTipo_proiezione(), listaProiezione.get(i).getPrezzo());
+
+                listaProiezioniFiltrate.add(proiezioneProva);
+
+            }
+
+        }
+
+        return listaProiezioniFiltrate;
+    }
+
 }
