94110201abe37692ed32c60baa8d7448340821c2
Yatin <Yatin@Yat-PC>
2016-05-03 14:04:21 +0200

    piccole modifiche!

--- Cinema/src/Gestore/PanelImpostazioni.java
+++ Cinema/src/Gestore/PanelImpostazioni.java
@@ -73,44 +73,10 @@ public class PanelImpostazioni extends JPanel {
             Logger.getLogger(Gui_Gestore.class.getName()).log(Level.SEVERE, null, ex);
         }
 
-                //Sei sicuro di volerlo mettere è un pò una ripetizione di questo stesso action listener
-//                refresh.addActionListener(new ActionListener() {
-//                    @Override
-//                    public void actionPerformed(ActionEvent ae) {
-//                        
-//                        try {
-//                            Config c c.add(gestore.aggiornaConfig());
-//                        } catch (SQLException ex) {
-//                            Logger.getLogger(Gui_Gestore.class.getName()).log(Level.SEVERE, null, ex);
-//                        }
-//                        text[0].setText(String.valueOf(c.get(0).getPrezzo_vip()));
-//                        text[1].setText(String.valueOf(c.get(0).getSconto()));
-//                        text[2].setText(String.valueOf(c.get(0).getPopcorn_s()));
-//                        text[3].setText(String.valueOf(c.get(0).getPopcorn_m()));
-//                        text[4].setText(String.valueOf(c.get(0).getPopcorn_l()));
-//                        text[5].setText(String.valueOf(c.get(0).getBibita_s()));
-//                        text[6].setText(String.valueOf(c.get(0).getBibita_m()));
-//                        text[7].setText(String.valueOf(c.get(0).getBibita_l()));
-//
-//                    }
-//                });
         submit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
 
-//                        String qry = "INSERT INTO Config(prezzo_vip,sconto,popcorn_s,popcorn_m,popcorn_l,bibita_s,bibita_m,bibita_l) VALUES("
-//                                + "'" + text[0].getText() + "','" + text[1].getText() + "','"
-//                                + text[2].getText() + "','" + text[3].getText() + "','" + text[4].getText() + "','"
-//                                + text[5].getText() + "','" + text[6].getText() + "','" + text[7].getText() + "')";
-//                        SQLConnessione db = new SQLConnessione();
-//                        db.creaConnessione();
-//                        try {
-//                            db.updateQuery(qry);
-//                        } catch (SQLException ex) {
-//                            Logger.getLogger(Gui_Gestore.class.getName()).log(Level.SEVERE, null, ex);
-//                        }
-//                        db.chiudiConnessione();
-//    public Config(double prezzo_vip, double sconto, double popcorn_s, double popcorn_m, double popcorn_l, double bibita_s, double bibita_m, double bibita_l) {
                 if (controller.scriviConfig(new Config(Double.parseDouble(text[0].getText()), Double.parseDouble(text[1].getText()), Double.parseDouble(text[2].getText()), Double.parseDouble(text[3].getText()), Double.parseDouble(text[4].getText()), Double.parseDouble(text[5].getText()), Double.parseDouble(text[6].getText()), Double.parseDouble(text[7].getText())))) {
                     outputGrafico.setText("Modifica registrata con successo.");
                 } else {
