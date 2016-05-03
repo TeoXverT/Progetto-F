e3c9eeed11ffac31cdd64c6896401cf65aa9c561
cl421572 <cl421572@aga24>
2016-05-03 17:48:24 +0200

    Aggiunta la possibilità di aggiungere un nuovo Film da interfaccia grafica. Aggiunta About

--- Cinema/src/Gestore/PanelAbout.java
+++ Cinema/src/Gestore/PanelAbout.java
@@ -34,6 +34,7 @@ public class PanelAbout extends JPanel {
         //infos.setPreferredSize(new java.awt.Dimension(384, 129));
         infos.setWrapStyleWord(true); //dovrebbe settare a capo automatico
         infos.setText(info);
+        infos.enable(false);
         panel.add(new JLabel(scalaImmagine(immagine, 640 / 3, 433 / 3)));
         panel.add(infos);
         this.add(panel);
