b3a306e6079457f383650aa06e47d9ecbc2261bc
pippo <pippo@Yat.homenet.telecomitalia.it>
2016-05-03 23:36:56 +0200

    Fatte le poltrone! e una piccola disposizione

--- Cinema/src/Gestore/PanelAddHall.java
+++ Cinema/src/Gestore/PanelAddHall.java
@@ -6,8 +6,14 @@
 package Gestore;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import oggetti.Seat;
 
 /**
  *
@@ -16,13 +22,36 @@ import javax.swing.JPanel;
 public class PanelAddHall extends JPanel {
     
     public PanelAddHall(Controller_Gestore controller, JLabel outputGrafico) {
+        
         this.setLayout(new BorderLayout());
         JPanel nord = new JPanel();
         JPanel sud = new JPanel();
-        JPanel seats_layout = new JPanel();
+        JPanel seats_layout = new JPanel(new GridLayout(5, 5, 1, 2));
+        
+        ImageIcon s = new ImageIcon("immagini/poltrone/seat_n.png");
+        JLabel[] seats = new JLabel[24];
+        for(int i = 0; i < 24; i++){
+                seats[i] = new JLabel(s);
+            }
+        
+//        System.out.println(seats.length);
+//        seats_layout.add(seats[0]);
+//        seats_layout.add(seats[1]);
+//        seat.addMouseListener(new MouseAdapter() {
+//            public void mouseClicked(MouseEvent m) {
+//                System.out.println("CLICCATO");
+//            }
+//        });
+        for(int i = 0; i < 24; i++) {
+            seats_layout.add(seats[i]);
+        }
         
         sud.add(seats_layout, BorderLayout.CENTER);
-        
         this.add(sud);
+        
+    }
+    
+    public void creaPosti() {
+        
     }
 }
