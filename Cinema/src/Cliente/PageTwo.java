326c13cdc011483a117ef1978ef1cef464b616d4
umeer <umeermohammad@gmail.com>
2016-05-04 18:52:06 +0200

    Aggiunta tasto indietro

--- Cinema/src/Cliente/PageTwo.java
+++ Cinema/src/Cliente/PageTwo.java
@@ -3,6 +3,7 @@ package Cliente;
 import chrriis.dj.nativeswing.swtimpl.NativeInterface;
 import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,11 +18,11 @@ import oggetti.Film;
  * @author Riccardo
  */
 public class PageTwo extends JPanel {
-
+    
     private Controller_Cliente controller;
     private Film film;
     private int deltaData;
-
+    
     public PageTwo(Film film, int deltaData, Controller_Cliente controller) {
         //Il controller ti permettera di parlare con il database
         //deltaData è l'offset in giorni rispetto ad oggi, ad es: se oggi è lunedi ed il tab da dove viene selezionato il film è martedi allora questo valore vale 1
@@ -30,38 +31,48 @@ public class PageTwo extends JPanel {
         this.controller = controller;
         this.film = film;
         this.deltaData = deltaData;
-        this.setLayout(new GridLayout(0, 2));
+        this.setLayout(new BorderLayout());
         crea_gui();
     }
-
+    
     private void crea_gui() {
-        this.add(new JLabel(film.toString(), SwingConstants.CENTER));
-        this.add(new JLabel(" " + deltaData, SwingConstants.CENTER));
+        this.add(new JLabel(" Titolo film: "+film.toString(), SwingConstants.CENTER), BorderLayout.WEST);
+        this.add(new JLabel(" Numero di GIorni dopo oggi: " + deltaData, SwingConstants.CENTER), BorderLayout.EAST);
 
         //Il native interface deve perforza essere chiuso prima di chiudere il programma, altrimenti crasha tutto
-        JButton cover = new JButton("Chiudi video");
-        this.add(cover);
+        JButton cover = new JButton("Indietro");
+        this.add(cover, BorderLayout.NORTH);
         cover.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 NativeInterface.close();
+                goBack();
             }
         });
-
-        this.add(JVideo(film.getLink_youtube()));
+        
+        this.add(JVideo(film.getLink_youtube()), BorderLayout.CENTER);
         this.revalidate();
         this.repaint();
     }
-
+    
+    private void goBack() {
+        this.removeAll();
+        this.setLayout(new BorderLayout());
+        this.add(new PageOne(controller));
+        this.revalidate();
+        this.repaint();
+    }
+    
     private JPanel JVideo(String link) {
         System.out.println(link);
         NativeInterface.open();
-
+        
         JPanel webBrowserPanel = new JPanel(new BorderLayout());
         JWebBrowser webBrowser = new JWebBrowser();
         webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
         webBrowser.setBarsVisible(false);
         webBrowser.navigate(link);
-
+        webBrowser.setMaximumSize(new Dimension(200, 200));
+        
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             @Override
             public void run() {
