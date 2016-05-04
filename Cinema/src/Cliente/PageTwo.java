f96842864549ec55ca55e813f930ab21b274f0b0
Riccardo <Riccardo@Riccardo-PC>
2016-05-04 12:57:16 +0200

    commit cliene page2 modifica 3

--- Cinema/src/Cliente/PageTwo.java
+++ Cinema/src/Cliente/PageTwo.java
@@ -7,8 +7,11 @@ package Cliente;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.sql.SQLException;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import oggetti.Film;
 
 /**
@@ -18,13 +21,22 @@ import oggetti.Film;
 public class PageTwo extends JPanel {
 
     Controller_Cliente controller;
-
-    public PageTwo(Film film, int deltaData, Controller_Cliente controller) {
+    Film film;
+    public PageTwo(Film film, int deltaData, Controller_Cliente controller) throws SQLException {
         this.controller = controller;
         this.setLayout(new GridLayout(0, 2));
         this.add(new JLabel(film.toString()));
         this.add(new JLabel(" " + deltaData));
-
+        /*this.film=film;
+        this.controller.InformazioniFilm(film.getId_film());
+        this.add(new JLabel(film.getLink_copertina()));
+        //getLink_youtube
+        this.add(new JLabel(film.getDescrizione()));
+        this.add(new JLabel(film.getTitolo_film()));
+        */
     }
-
+    
 }
+
+
+
