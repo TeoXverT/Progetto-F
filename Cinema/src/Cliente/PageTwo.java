f62c4029a5d35cb2489908e6811ec3cfb0bdd5ba
Riccardo <Riccardo@Riccardo-PC.homenet.telecomitalia.it>
2016-05-05 19:41:52 +0200

    modifiche gui page 2

--- Cinema/src/Cliente/PageTwo.java
+++ Cinema/src/Cliente/PageTwo.java
@@ -1,14 +1,25 @@
 package Cliente;
 
 import java.awt.BorderLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import oggetti.Film;
-import oggetti.PanelYoutube;
+import oggetti.Proiezione;
+
 
 /**
  *
@@ -19,7 +30,7 @@ public class PageTwo extends JPanel {
     private Controller_Cliente controller;
     private Film film;
     private int deltaData;
-
+    private ArrayList<Proiezione> proiezione;
     public PageTwo(Film film, int deltaData, Controller_Cliente controller) {
         //Il controller ti permettera di parlare con il database
         //deltaData è l'offset in giorni rispetto ad oggi, ad es: se oggi è lunedi ed il tab da dove viene selezionato il film è martedi allora questo valore vale 1
@@ -35,13 +46,42 @@ public class PageTwo extends JPanel {
 
     private void crea_gui() {
         JButton cover = new JButton("Indietro");
-        this.add(cover, BorderLayout.NORTH);
+     
+        this.add(cover, BorderLayout.SOUTH);
+        
         cover.addActionListener(goBackEvent());
+        
+        this.add(new JLabel(" Titolo film: " + film.toString(), SwingConstants.CENTER), BorderLayout.NORTH);
+        this.add(new JLabel(" Numero di Giorni dopo oggi: " + deltaData, SwingConstants.CENTER), BorderLayout.EAST);
+        this.add(new JLabel(film.getDescrizione()), BorderLayout.WEST);
+        
+        Image image = null;
+try {
+    URL url = new URL(film.getLink_copertina());
+    image = ImageIO.read(url);
+} catch (IOException e) {
+}
+this.add(new JLabel (new ImageIcon(image)), BorderLayout.NORTH);        
+        String ora = null; //ora sarà passata dallo slider in page 1
+        try {        
+         //           this.add(new PanelYoutube(film.getLink_youtube(), 200, 200), BorderLayout.CENTER);  da errore
+         proiezione=controller.showByFilm(film.getId_film(), deltaData, ora);
+        } catch (SQLException ex) {
+            Logger.getLogger(PageTwo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+  /*  for(int i = 0; i < proiezione.size(); i++){
 
-        this.add(new JLabel(" Titolo film: " + film.toString(), SwingConstants.CENTER), BorderLayout.WEST);
-        this.add(new JLabel(" Numero di GIorni dopo oggi: " + deltaData, SwingConstants.CENTER), BorderLayout.EAST);
-        this.add(new PanelYoutube(film.getLink_youtube(), 200, 200), BorderLayout.CENTER);
+     this.add(new JLabel(proiezione.get(i).getTipo_proiezione()), BorderLayout.SOUTH);
+     this.add(new JLabel((Icon) proiezione.get(i).getData_ora()), BorderLayout.SOUTH);
+     this.add(new JLabel(proiezione.get(i).getId_sala()), BorderLayout.SOUTH);
+     
+}*/
+  
     }
+  
+
+      
+    
 
     private void goBack() {
         this.removeAll();
@@ -58,6 +98,5 @@ public class PageTwo extends JPanel {
                 goBack();
             }
         };
-        return evento;
-    }
-}
+        return evento;}}
+    
