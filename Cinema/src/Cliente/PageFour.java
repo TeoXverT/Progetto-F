39e75e01012c90f422067e807db67d808b3f0780
umeer <umeermohammad@gmail.com>
2016-05-20 19:33:28 +0200

    Finita page 4

--- Cinema/src/Cliente/PageFour.java
+++ Cinema/src/Cliente/PageFour.java
@@ -1,7 +1,21 @@
-
 package Cliente;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.SQLException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import oggetti.*;
 
 /**
@@ -9,21 +23,162 @@ import oggetti.*;
  * @author Yoga
  */
 public class PageFour extends JPanel {
-    
+
     Controller_Cliente controller;
     Film film;
     Proiezione proiezione;
     Prenotazione prenotazione;
+    Config config;
+    JLabel totalPrice = new JLabel();
+    ExtraElement[] spinner = new ExtraElement[6];
 
-    public PageFour(Film film, Proiezione proiezione, Prenotazione prenotazione,Controller_Cliente controller) {
+    public PageFour(Film film, Proiezione proiezione, Prenotazione prenotazione, Controller_Cliente controller, Config config) {
         this.controller = controller;
         this.film = film;
         this.proiezione = proiezione;
         this.prenotazione = prenotazione;
+        this.config = config;
+
+        drawGui();
     }
-    
-    
-    
-    
-    
+
+    private void drawGui() {
+        this.setLayout(new BorderLayout());
+
+        JPanel pannelloCopertina = new JPanel();
+        try {
+            URL url = new URL(film.getLink_copertina());
+            Image image = ImageIO.read(url);
+            JLabel label1 = new JLabel(scalaImmagine(new ImageIcon(image), 210, 300));
+            pannelloCopertina.add(label1);
+        } catch (IOException e) {
+        }
+        this.add(pannelloCopertina, BorderLayout.NORTH);
+
+        JPanel centro = new JPanel(new GridLayout(0, 1));
+
+        JPanel carrello = new JPanel(new GridLayout(0, 2));
+        carrello.add(new JLabel("<html><h3>Proiezione il " + proiezione.getData_ora_friendly_2() + "</h3></html>"));
+        carrello.add(new JLabel("<html><h3>Nella Sala " + proiezione.getId_sala() + "</h3></html>"));
+
+        carrello.add(new JLabel("<html><b><h3>Carrello:</h2></b><html>"));
+        carrello.add(new JLabel(""));
+
+        for (Seat s : prenotazione.getPosti_prenotati()) {
+            carrello.add(new JLabel("<html><font size=\"5\"><i> Posto " + s.getx() + " Fila " + s.gety() + "</font></html>"));
+            carrello.add(new JLabel(""));
+
+        }
+
+        JPanel price = new JPanel(new GridLayout(1, 2));
+
+        spinner[0] = new ExtraElement("Pop-corn - SMALL", config.getPopcorn_s(), new SpinnerNumberModel(0, 0, 5, 1));
+        spinner[0].addChangeListener(updateTotal());
+        price.add(spinner[0]);
+        price.add(new JLabel("X " + spinner[0].getPrice() + " €"));
+        carrello.add(new JLabel("<html><font size=\"4\"><i>" + spinner[0].getName() + "</font></html>"));
+        carrello.add(price);
+
+        price = new JPanel(new GridLayout(1, 2));
+        spinner[1] = new ExtraElement("Pop-corn - MEDIUM", config.getPopcorn_m(), new SpinnerNumberModel(0, 0, 5, 1));
+        spinner[1].addChangeListener(updateTotal());
+        price.add(spinner[1]);
+        price.add(new JLabel("X " + spinner[1].getPrice() + " €"));
+        carrello.add(new JLabel("<html><font size=\"4\"><i>" + spinner[1].getName() + "</font></html>"));
+        carrello.add(price);
+
+        price = new JPanel(new GridLayout(1, 2));
+        spinner[2] = new ExtraElement("Pop-corn - LARGE", config.getPopcorn_l(), new SpinnerNumberModel(0, 0, 5, 1));
+        spinner[2].addChangeListener(updateTotal());
+        price.add(spinner[2]);
+        price.add(new JLabel("X " + spinner[2].getPrice() + " €"));
+        carrello.add(new JLabel("<html><font size=\"4\"><i>" + spinner[2].getName() + "</font></html>"));
+        carrello.add(price);
+
+        price = new JPanel(new GridLayout(1, 2));
+        spinner[3] = new ExtraElement("Drink - SMALL", config.getBibita_s(), new SpinnerNumberModel(0, 0, 5, 1));
+        spinner[3].addChangeListener(updateTotal());
+        price.add(spinner[3]);
+        price.add(new JLabel("X " + spinner[3].getPrice() + " €"));
+        carrello.add(new JLabel("<html><font size=\"4\"><i>" + spinner[3].getName() + "</font></html>"));
+        carrello.add(price);
+
+        price = new JPanel(new GridLayout(1, 2));
+        spinner[4] = new ExtraElement("Drink - MEDIUM", config.getBibita_m(), new SpinnerNumberModel(0, 0, 5, 1));
+        spinner[4].addChangeListener(updateTotal());
+        price.add(spinner[4]);
+        price.add(new JLabel("X " + spinner[4].getPrice() + " €"));
+        carrello.add(new JLabel("<html><font size=\"4\"><i>" + spinner[4].getName() + "</font></html>"));
+        carrello.add(price);
+
+        price = new JPanel(new GridLayout(1, 2));
+        spinner[5] = new ExtraElement("Drink - LARGE", config.getBibita_l(), new SpinnerNumberModel(0, 0, 5, 1));
+        spinner[5].addChangeListener(updateTotal());
+        price.add(spinner[5]);
+        price.add(new JLabel("X " + spinner[5].getPrice() + " €"));
+        carrello.add(new JLabel("<html><font size=\"4\"><i>" + spinner[5].getName() + "</font></html>"));
+        carrello.add(price);
+
+        carrello.add(new JLabel("<html><font size=\"4\"><i>Total</font></html>"));
+        totalPrice.setText("<html><font size=\"6\">" + prenotazione.getPrezzo() + " €</font></html>");
+        carrello.add(totalPrice);
+        centro.add(carrello);
+
+        this.add(centro, BorderLayout.CENTER);
+
+        JButton next = new JButton("Next");
+        next.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                try {
+                    double newPrice = 0;
+                    for (int i = 0; i < 6; i++) {
+                        newPrice = newPrice + spinner[i].getTotalPrice();
+                    }
+                    prenotazione.setPrezzo(newPrice + prenotazione.getPrezzo());
+//                    openPage(new PageFive(prenotazione,controller));
+                    openPage(new PageOne(controller));
+                } catch (SQLException ex) {
+                }
+            }
+        });
+        this.add(next, BorderLayout.AFTER_LINE_ENDS);
+
+        JButton back = new JButton("Back");
+        back.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                try {
+                    openPage(new PageOne(controller));
+                } catch (SQLException ex) {
+                }
+            }
+        });
+        this.add(back, BorderLayout.BEFORE_LINE_BEGINS);
+    }
+
+    private ImageIcon scalaImmagine(ImageIcon immagine, int lunghezza, int altezza) {
+        return new ImageIcon(immagine.getImage().getScaledInstance(lunghezza, altezza, java.awt.Image.SCALE_SMOOTH));
+    }
+
+    private ChangeListener updateTotal() {
+        ChangeListener evento = new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                double newPrice = 0;
+                for (int i = 0; i < 6; i++) {
+                    newPrice = newPrice + spinner[i].getTotalPrice();
+                }
+                newPrice = newPrice + prenotazione.getPrezzo();
+                totalPrice.setText("<html><font size=\"6\">" + newPrice + " €</font></html>");
+            }
+        };
+        return evento;
+    }
+
+    private void openPage(JPanel panel) throws SQLException {
+        this.removeAll();
+        this.setLayout(new BorderLayout());
+        this.add(panel);
+        this.revalidate();
+        this.repaint();
+    }
 }
--- Cinema/src/Cliente/PageTwo.java
+++ Cinema/src/Cliente/PageTwo.java
@@ -31,24 +31,27 @@ import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 import javax.swing.border.Border;
 import oggetti.ButtonCart;
+import oggetti.Config;
 import oggetti.Film;
 import oggetti.PanelYoutube;
+import oggetti.Prenotazione;
 import oggetti.Proiezione;
+import oggetti.Seat;
 
 /**
  *
  * @author Riccardo
  */
 public class PageTwo extends JPanel {
-
+    
     private Controller_Cliente controller;
     private Film film;
     private int deltaData;
     private ArrayList<Proiezione> proiezione;
     private int deltaTime;
-
     
     Proiezione proiezione1;
+    
     public PageTwo(Film film, int deltaData, Controller_Cliente controller, int deltaTime) throws SQLException, IOException {
         //Il controller ti permettera di parlare con il database
         //deltaData è l'offset in giorni rispetto ad oggi, ad es: se oggi è lunedi ed il tab da dove viene selezionato il film è martedi allora questo valore vale 1
@@ -61,12 +64,12 @@ public class PageTwo extends JPanel {
         this.deltaTime = deltaTime;
         this.setLayout(new GridLayout(2, 2, 2, 2));
         crea_gui();
-
+        
     }
-
+    
     private void crea_gui() throws SQLException, MalformedURLException, IOException {
         JButton cover = new JButton("Indietro");
-
+        
         JPanel pannelloCopertina = new JPanel();
         pannelloCopertina.setBackground(java.awt.Color.LIGHT_GRAY);
         this.add(pannelloCopertina);
@@ -79,16 +82,15 @@ public class PageTwo extends JPanel {
         ImageIcon ii = new ImageIcon(image);
         JLabel label1 = new JLabel(scalaImmagine(ii, 250, 350));
         pannelloCopertina.add(label1);
-       
+        
         cover.addActionListener(goBackEvent());
-
+        
         JPanel pannelloTrama = new JPanel(new GridBagLayout());
         pannelloTrama.setBackground(java.awt.Color.LIGHT_GRAY);
         GridBagConstraints c = new GridBagConstraints();
-       
-        this.setBackground(java.awt.Color.GRAY);
-        this.add(pannelloTrama );
         
+        this.setBackground(java.awt.Color.GRAY);
+        this.add(pannelloTrama);
 
         //prima riga terza colonna
         c.fill = GridBagConstraints.HORIZONTAL;
@@ -97,26 +99,23 @@ public class PageTwo extends JPanel {
         c.gridy = 0;
         c.weightx = 0.5;
         
-        
         pannelloTrama.add(cover, c);
-        
-        
+
         //prima riga seconda colonna
-       c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 1;
         c.weightx = 0.5;
         c.gridy = 0;
         c.anchor = GridBagConstraints.PAGE_START;
         pannelloTrama.add(new JLabel(film.toString()), c);
-        
-        
+
         //trama
         JTextArea Trama = new JTextArea(film.getDescrizione());
         Trama.setLineWrap(true);
         Trama.setEditable(false);
         Trama.setBorder(null);
         Trama.setBackground(java.awt.Color.LIGHT_GRAY);
-      
+        
         c.anchor = GridBagConstraints.CENTER;
         c.fill = GridBagConstraints.HORIZONTAL;
         c.gridx = 0;
@@ -126,9 +125,9 @@ public class PageTwo extends JPanel {
         c.ipady = 30;
         
         pannelloTrama.add(Trama, c);
-
+        
         this.add(new PanelYoutube(film.getLink_youtube(), 200, 200));
-
+        
         proiezione = controller.showByFilm(film.getId_film(), deltaData, deltaTime);
         SimpleDateFormat sdfDate = new SimpleDateFormat("HH:mm:ss");
 
@@ -139,12 +138,11 @@ public class PageTwo extends JPanel {
         JPanel pannelloOrari = new JPanel(new GridLayout(proiezione.size(), 2, 10, 10));
         pannelloOrari.setBackground(java.awt.Color.LIGHT_GRAY);
         pannelloContenitoreBackOrari.add(pannelloOrari, BorderLayout.CENTER);
-      
-
+        
         ButtonCart bottoneCarrello;
         
         for (int i = 0; i < proiezione.size(); i++) {
-
+            
             pannelloOrari.add(new JLabel(sdfDate.format(proiezione.get(i).getData_ora().getTime()) + "     " + proiezione.get(i).getTipo_proiezione() + "     " + proiezione.get(i).getId_sala()), BorderLayout.SOUTH);
             bottoneCarrello = new ButtonCart(proiezione.get(i));
             bottoneCarrello.setPreferredSize(new Dimension(50, 50));
@@ -154,7 +152,7 @@ public class PageTwo extends JPanel {
             bottoneCarrello.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent ae) {
                     try {
-                        openPageThree(film,proiezione1,controller);
+                        openPageThree(film, proiezione1, controller);
                     } catch (SQLException ex) {
                         Logger.getLogger(PageTwo.class.getName()).log(Level.SEVERE, null, ex);
                     }
@@ -162,17 +160,15 @@ public class PageTwo extends JPanel {
             });
             
             pannelloOrari.add(bottoneCarrello);
-
+            
         }
-
-      
-
+        
     }
-
+    
     private ImageIcon scalaImmagine(ImageIcon immagine, int lunghezza, int altezza) {
         return new ImageIcon(immagine.getImage().getScaledInstance(lunghezza, altezza, java.awt.Image.SCALE_SMOOTH));
     }
-
+    
     private void goBack() {
         this.removeAll();
         this.setLayout(new BorderLayout());
@@ -180,7 +176,7 @@ public class PageTwo extends JPanel {
         this.revalidate();
         this.repaint();
     }
-
+    
     private ActionListener goBackEvent() {
         ActionListener evento = new ActionListener() {
             @Override
@@ -191,18 +187,22 @@ public class PageTwo extends JPanel {
         return evento;
     }
     
-    
     private void openPageThree(Film film, Proiezione proiezione, Controller_Cliente controller) throws SQLException {
         this.removeAll();
-        this.add(new PageThree(proiezione, controller));
+        this.setLayout(new BorderLayout());
+//        this.add(new PageThree(proiezione, controller));
+
+        ///------------  prova della page 4
+        ArrayList<Seat> posti_prenotati = new ArrayList<>();
+        posti_prenotati.add(new Seat(340, 12, 13));
+        posti_prenotati.add(new Seat(341, 14, 13));
+        Prenotazione prenotazione = new Prenotazione(0, 18, posti_prenotati, null, 20);
+        this.add(new PageFour(film, proiezione, prenotazione, controller,new Config(1,2,3,4,5,6,7,8,9)),BorderLayout.CENTER);
+        ///------------  prova della page 4
+        
+            
         this.revalidate();
         this.repaint();
-        
-        
     }
     
-    
-    
-    
-
 }
