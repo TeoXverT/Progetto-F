1c9416be5a2925b818ffbd70750aabb2ee29d078
p0rch <p0rch@PORCH-3301>
2016-06-08 00:03:41 +0200

    sviluppo gestione fatturato

--- Cinema/src/Gestore/PanelSalesVolume.java
+++ Cinema/src/Gestore/PanelSalesVolume.java
@@ -10,8 +10,11 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import static java.util.Calendar.HOUR_OF_DAY;
 import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -51,8 +54,8 @@ public class PanelSalesVolume extends JPanel {
         JPanel north = new JPanel(new FlowLayout(FlowLayout.CENTER));
         final JButton search = new JButton("Search");
         final Date today = new Date();
-        final JSpinner dateFrom = new JSpinner(new SpinnerDateModel(setDate(today, 1), null, today, Calendar.MONTH));
-        final JSpinner dateTo = new JSpinner(new SpinnerDateModel(today, null, today, Calendar.MONTH));
+        final JSpinner dateFrom = new JSpinner(new SpinnerDateModel(setDate(today, 1, 0), null, today, Calendar.MONTH));
+        final JSpinner dateTo = new JSpinner(new SpinnerDateModel(setDate(today,0,1), null, today, Calendar.MONTH));
         JSpinner.DateEditor formatFrom = new JSpinner.DateEditor(dateFrom, "dd-MM-yyyy");
         JSpinner.DateEditor formatTo = new JSpinner.DateEditor(dateTo, "dd-MM-yyyy");
         dateFrom.setEditor(formatFrom);
@@ -94,7 +97,11 @@ public class PanelSalesVolume extends JPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    ArrayList<Prenotazione> books = c.salesVolume("a", "s");
+                    ArrayList<Prenotazione> books = c.salesVolume(dateToString((Date)dateFrom.getValue()), dateToString((Date)dateTo.getValue()));
+                    System.out.println(dateToString((Date)dateTo.getValue()));                               
+                    double tot = tableUpdateAndCount(books);
+                    total.setText(String.valueOf(tot));
+                    
                 } catch (SQLException ex) {
                     Logger.getLogger(PanelSalesVolume.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -105,10 +112,10 @@ public class PanelSalesVolume extends JPanel {
         return north;
     }
     
+    String[] columnNames = {"ID", "Date & Time", "3D Glasses", "Price"};
+    DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
     private JPanel Center(){
         JPanel center = new JPanel();
-        String[] columnNames = {"ID", "Date & Time", "Price"};
-        DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
         JTable table = new JTable(tableModel);
         TableColumn id = table.getColumnModel().getColumn(0);
         id.setMinWidth(40); id.setMaxWidth(40); id.setPreferredWidth(40);
@@ -118,10 +125,10 @@ public class PanelSalesVolume extends JPanel {
         return center;
     }
     
+    final JTextField total = new JTextField("0", 10);
     private JPanel South(){
         JPanel south = new JPanel();
         JLabel tot = new JLabel("Tot: ");
-        final JTextField total = new JTextField("0", 10);
         total.setEditable(false); 
         JButton save = new JButton("Save");
         save.setPreferredSize(new Dimension(120, 30));
@@ -152,14 +159,44 @@ public class PanelSalesVolume extends JPanel {
     }
     
 //******************************************************************************
-    private Date setDate(Date date, int month) {
+    private Date setDate(Date date, int month, int check) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
-        cal.add(Calendar.MONTH, -month);
-        return cal.getTime();
+        if(check==0){ //FROM
+            cal.add(Calendar.MONTH, -month);
+            cal.set(Calendar.HOUR_OF_DAY, 0);
+            cal.set(Calendar.MINUTE, 0);
+            cal.set(Calendar.SECOND, 0);
+            cal.set(Calendar.MILLISECOND, 0);
+            return cal.getTime();
+        }else{        //TO
+            cal.set(Calendar.HOUR_OF_DAY, 22);
+            cal.set(Calendar.MINUTE, 59);
+            cal.set(Calendar.SECOND, 59);
+            cal.set(Calendar.MILLISECOND, 0);
+            
+            return cal.getTime();
+        }
     }
+    
     public long getTimeDiff(Date dateOne, Date dateTwo) {
         long timeDiff = (dateOne.getTime() - dateTwo.getTime());
         return timeDiff;
+    }
+    
+    public double tableUpdateAndCount(ArrayList<Prenotazione> x){
+        tableModel.setRowCount(0);
+        double tot = 0D;
+        for (Prenotazione p : x) {      
+            Object[] datas = {p.getId_prenotazione(), p.getData_ora_sql(),p.getNumber_of_glasses(), p.getPrezzo()}; 
+            tot += p.getPrezzo();
+            tableModel.addRow(datas);
+        }
+        return tot;
+    }       
+    
+    public String dateToString(Date date){
+        Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return formatter.format(date);
     }
 }
