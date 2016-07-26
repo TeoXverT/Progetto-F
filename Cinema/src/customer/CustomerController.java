f4202fbfce43ab165724701ed787debf315f8849
Cris <Cris@Cri>
2016-07-26 11:41:20 +0200

    tolto codice commentato inutile, tolti inused import e sistemato ordine codice

--- Cinema/src/customer/CustomerController.java
+++ Cinema/src/customer/CustomerController.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package customer;
 
 import inputoutput.AdapterSQLCustomer;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.TimeZone;
 import obj.Config;
 import obj.Film;
 import obj.Booking;
@@ -17,16 +12,10 @@ import obj.Projection;
 import obj.Hall;
 import obj.Seat;
 
-/**
- *
- * @author Yoga
- */
 public class CustomerController {
 
     private static CustomerController instance;
 
-//    private ArrayList<Projection> listaProiezioniFuture;
-//    private ArrayList<Projection> listaProiezioniFiltrate;
     private AdapterSQLCustomer adapter;
     private Config config;
 
@@ -41,38 +30,6 @@ public class CustomerController {
         return instance;
     }
 
-//    public ArrayList<Projection> listFutureProjections(ArrayList<Projection> projection) {
-//        int i;
-//        Calendar giornoAttuale;
-//        TimeZone timezone = TimeZone.getTimeZone("Europe/Rome");
-//        Projection proiezioneProva;
-//        ArrayList<Projection> futureProjection = new ArrayList<>();
-//        giornoAttuale = Calendar.getInstance(timezone);
-//        for (i = 0; i < projection.size(); i++) {
-//            if (projection.get(i).getData_ora().after(giornoAttuale)) {
-//                proiezioneProva = new Projection(projection.get(i).getIdProjection(), projection.get(i).getData_ora(), projection.get(i).getFilm(), projection.get(i).getRoom(), projection.get(i).getpProjectionType(), projection.get(i).getPrice());
-//                futureProjection.add(proiezioneProva);
-//            }
-//        }
-//        return futureProjection;
-//    }
-
-//    public ArrayList<Projection> listFilteredProjections(ArrayList<Projection> listaProiezione, int GiornoSettimana) {
-//        int i;
-//        Calendar giornoAttuale;
-//        TimeZone timezone = TimeZone.getTimeZone("Europe/Rome");
-//        Projection proiezioneProva;
-//        ArrayList<Projection> listaProiezioniFiltrate = new ArrayList<>();
-//        giornoAttuale = Calendar.getInstance(timezone);
-//        for (i = 0; i < listaProiezione.size(); i++) {
-//            System.out.println(listaProiezione.get(i).getData_ora().get(Calendar.DAY_OF_WEEK));
-//            if (listaProiezione.get(i).getData_ora().after(giornoAttuale) && (listaProiezione.get(i).getData_ora().get(Calendar.DAY_OF_WEEK)) == GiornoSettimana) {
-//                proiezioneProva = new Projection(listaProiezione.get(i).getIdProjection(), listaProiezione.get(i).getData_ora(), listaProiezione.get(i).getFilm(), listaProiezione.get(i).getRoom(), listaProiezione.get(i).getpProjectionType(), listaProiezione.get(i).getPrice());
-//                listaProiezioniFiltrate.add(proiezioneProva);
-//            }
-//        }
-//        return listaProiezioniFiltrate;
-//    }
 
     public ArrayList<Film> futureFilmBySlider(int deltaData, int sliderValue) throws SQLException {
         return adapter.getIncomingFilmBySlider(deltaData, sliderValue);
--- Cinema/src/customer/CustomerGui.java
+++ Cinema/src/customer/CustomerGui.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package customer;
 
 import java.awt.BorderLayout;
@@ -10,10 +5,6 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-/**
- *
- * @author Yoga
- */
 public class CustomerGui extends JFrame {
 
     JPanel display = new JPanel(new BorderLayout());
