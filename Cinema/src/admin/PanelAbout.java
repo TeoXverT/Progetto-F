52416b501c94c167d31e359bd57c61feb62e6ac9
p0rch <p0rch@PORCH-3301>
2016-07-26 09:53:31 +0200

    In inglese

--- Cinema/src/admin/PanelAbout.java
+++ Cinema/src/admin/PanelAbout.java
@@ -22,8 +22,8 @@ public class PanelAbout extends JPanel {
     public PanelAbout(final AdminController controller, final JLabel outputGrafico) {
         JPanel panel = new JPanel(new GridLayout(0, 1, 20, 20));
         ImageIcon immagine = new ImageIcon("images/logo.png");
-        String info = "PROGETTO F - v2.023 <Beta>\n" + " \n";
-        info += "Sviluppato da: \n";
+        String info = "PROGETTO F aka CINEMA - v11.02 <Stable>\n" + " \n";
+        info += "Development Team: \n";
         info += "Umeer Mohammad \n";
         info += "Riccardo Previtali \n";
         info += "Cristian Santangelo \n";
@@ -35,12 +35,12 @@ public class PanelAbout extends JPanel {
         infos.setWrapStyleWord(true); //dovrebbe settare a capo automatico
         infos.setText(info);
         infos.setEnabled(false);
-        panel.add(new JLabel(scalaImmagine(immagine, 640 / 2, 433 / 2)));
+        panel.add(new JLabel(imgResize(immagine, 640 / 2, 433 / 2)));
         panel.add(infos);
         this.add(panel);
     }
         
-   public ImageIcon scalaImmagine(ImageIcon immagine, int lunghezza, int altezza) {
+   public ImageIcon imgResize(ImageIcon immagine, int lunghezza, int altezza) {
         return new ImageIcon(immagine.getImage().getScaledInstance(lunghezza, altezza, java.awt.Image.SCALE_SMOOTH));
     }
 
