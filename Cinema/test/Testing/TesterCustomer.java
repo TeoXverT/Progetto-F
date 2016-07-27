ae9cbf92980b6bd57f52990185033cfe26d7d854
Cris <Cris@Cri>
2016-07-27 12:40:32 +0200

    aggiunta testercustomer

---
+++ Cinema/test/Testing/TesterCustomer.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Testing;
+
+import customer.CustomerController;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import obj.Booking;
+import obj.Hall;
+import obj.Seat;
+import static org.hamcrest.CoreMatchers.is;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Cris
+ */
+public class TesterCustomer {
+    
+    public TesterCustomer() {
+    }
+    
+    @Test
+    public void eliminazioneBiglettiNonPagati() {
+        CustomerController c = CustomerController.getInstance();
+        try {
+           
+             assertThat( c.writeBooking(new Booking(1, null, null, null, 0, 0, 0, ""))==0 , is(true));
+        } catch (SQLException ex) {
+            Logger.getLogger(TesterAdmin.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
