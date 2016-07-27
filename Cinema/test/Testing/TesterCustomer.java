44f03088773d0069d488c3727092f87513ee14af
Cris <Cris@Cri>
2016-07-27 14:26:54 +0200

    modifiche testcustomer

--- Cinema/test/Testing/TesterCustomer.java
+++ Cinema/test/Testing/TesterCustomer.java
@@ -2,18 +2,11 @@ package Testing;
 
 import customer.CustomerController;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import obj.Booking;
-import obj.Hall;
-import obj.Seat;
 import static org.hamcrest.CoreMatchers.is;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
