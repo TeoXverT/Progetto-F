b6b33c0796f70964940d97d2a56cb7cc3fe36c2e
Cris <Cris@Cri>
2016-07-27 21:12:14 +0200

    aggiunti test admin

--- Cinema/test/Testing/TesterAdmin.java
+++ Cinema/test/Testing/TesterAdmin.java
@@ -6,70 +6,47 @@
 package Testing;
 
 import admin.AdminController;
-import customer.CustomerController;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import obj.Booking;
 import obj.Config;
-import obj.Film;
-import obj.Hall;
 import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author Yoga
- */
+
 public class TesterAdmin {
-
+    AdminController c;
     public TesterAdmin() {
+        
+        c = AdminController.getInstance();
+        char[] codice = {'1', '2', '3'};
+        c.codeVerification(codice);
+        
     }
 
-   
     @Test
-    public void writeFilm() {
-        //testo scrittura film
-        AdminController c = AdminController.getInstance();
-        assertThat( c.writeFilm(new Film(null, null, 0, null, null, null)) == false , is(true));
-    }
-    
-    
-    @Test
-    public void test2() throws SQLException {
-        
-         AdminController c = AdminController.getInstance();
-         assertThat(c.getFilm(0).isEmpty(), is(false));
-        
-    }
-    
-    
-    @Test
-    public void test3() throws SQLException {
-        
-        AdminController c = AdminController.getInstance();
+    public void writeconfig() throws SQLException {
+       
         assertThat(c.writeConfig(new Config(0, 0, 0, 0, 0)) == false, is(true));
         
     }
     
-    
-   
-    
     @Test
-    public void test5() throws SQLException {
-        AdminController c = AdminController.getInstance();
-        ArrayList<Hall> hall = new ArrayList<Hall>();
-        hall = c.getHall();
-       
-        assertThat(hall.isEmpty(), is(false));
+    public void getBooking()  {
+        
+        ArrayList<Booking> booking = new ArrayList<>();
+        
+        try {
+            booking = c.getBooking();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        assertThat(booking.isEmpty(), is(true));
         
         
     }
-    
-    
-    
     
 }
